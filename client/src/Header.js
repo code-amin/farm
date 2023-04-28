8525413dee1fd4d749a1bc10592e9ee1352062b5
“Amin <73975219+code-amin@users.noreply.github.com>
2023-04-28 19:16:12 -0400

    first template of project

---
+++ client/src/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>Headaaaer</div>;
+};
+
+export default Header;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const helmet = require("helmet");
+const express = require("express");
+const morgan = require("morgan");
+
+express()
+  .use(helmet())
+  .use(morgan("tiny"))
+
+  .get("/hello", (req, res) => {
+    res.status(200).json({ status: 200, message: "hi!" });
+  })
+
+  .listen(8000, () => {
+    console.log(`Example app listening on port 8000}`);
+  });
