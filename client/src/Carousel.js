19993b20505852c49843cc4f6af5ff5ea22ad883
“Amin <73975219+code-amin@users.noreply.github.com>
2023-07-11 12:06:16 -0400

    images

--- client/src/Carousel.js
+++ client/src/Carousel.js
@@ -2,6 +2,7 @@ import { BsChevronCompactRight } from "react-icons/bs";
 import { BsChevronCompactLeft } from "react-icons/bs";
 import { useState } from "react";
 
+//Tailwind CSS is broken, inline style because client is urged.
 const Carousel = () => {
   const slides = [
     {
@@ -29,20 +30,68 @@ const Carousel = () => {
     setCurrentIndex(newIndex);
   };
   return (
-    <div className="py-20">
-      <div className="max-w-[700px] h-[600px] w-full m-auto py-16 px-4 relative ">
+    <div className="py-20 ">
+      <div
+        className="max-w-[700px] h-[600px] w-full m-auto py-16 px-4 relative "
+        style={{
+          position: "relative",
+          paddingLeft: "1rem",
+          paddingRight: "1rem",
+          paddingTop: "4rem",
+          paddingBottom: "4rem",
+          width: "100%",
+          maxWidth: "700px",
+          height: "600px",
+        }}
+      >
         <div
           id="carousel"
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+          style={{
+            backgroundImage: `url(${slides[currentIndex].url})`,
+            borderRadius: "1rem",
+            width: "100%",
+            height: "100%",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            transitionDuration: "500ms",
+            scrollSnapType: "x var(--tw-scroll-snap-strictness)",
+          }}
           className="w-full h-full rounded-2xl bg-center bg-cover duration-500 scroll snap-x"
         ></div>
 
         {/* Left Arrow */}
-        <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/30 text-white cursor-pointer hover:bg-gray-400/30 ">
+        <div
+          className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/30 text-white cursor-pointer hover:bg-gray-400/30 "
+          style={{
+            position: "absolute",
+            left: "1.25rem",
+            padding: "0.5rem",
+            borderRadius: "9999px",
+            fontSize: "1.5rem",
+            lineHeight: "2rem",
+            color: "#ffffff",
+            cursor: "pointer",
+            top: "50%",
+          }}
+        >
           <BsChevronCompactLeft onClick={prevSlide} size={50} />
         </div>
         {/* Right Arrow */}
-        <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/30 text-white cursor-pointer hover:bg-gray-400/30 ">
+        <div
+          className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/30 text-white cursor-pointer hover:bg-gray-400/30 "
+          style={{
+            position: "absolute",
+            right: "1.25rem",
+            padding: "0.5rem",
+            borderRadius: "9999px",
+            fontSize: "1.5rem",
+            lineHeight: "2rem",
+            color: "#ffffff",
+            cursor: "pointer",
+            top: "50%",
+            ":hover": {},
+          }}
+        >
           <BsChevronCompactRight onClick={nextSlide} size={50} />
         </div>
       </div>
