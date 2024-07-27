028218488317e9c01fead5c7e17e0e80710cc851
AlphaHelloWorld <73975219+AlphaHelloWorld@users.noreply.github.com>
2024-07-27 16:24:56 -0400

    add button, notch, rtoutes

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,16 @@
 const Home = () => {
   return (
-<div className="BANNER bg-[rgb(241,243,216)] pt-20 px-20 pb-[100vh] drop-shadow-2xl flex items-center justify-center">
+<div className="BANNER  pt-20 px-20 drop-shadow-2xl flex flex-col items-center justify-center">
   <img
     className="min-w-[400px] sm:w-full rounded-lg shadow-2xl max-w-[1000px] object-contain"
     src="https://raw.githubusercontent.com/code-amin/farm/e44f199181acf0c85490be6b467d83bd3bbc25e4/client/src/images/image0.jpeg"
     alt="Descriptive Alt Text"
   />
+      
+  <a href="/#/home"
+    className="DETAILS flex items-center justify-center my-4 px-4 py-2 sm:px-2 sm:py-2 font-medium rounded-lg text-black bg-white border-2 border-black hover:bg-gray-200 whitespace-nowrap text-base sm:text-lg md:text-xl lg:text-2xl">
+    Détails
+  </a>
 </div>
 
   );
