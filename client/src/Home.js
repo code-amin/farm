06b88452180bc65c39a0c8022215766561b13ebb
AlphaHelloWorld <73975219+AlphaHelloWorld@users.noreply.github.com>
2024-07-27 16:30:06 -0400

    fix w on mobile

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
 <div className="BANNER  pt-20 px-20 drop-shadow-2xl flex flex-col items-center justify-center">
   <img
-    className="min-w-[400px] sm:w-full rounded-lg shadow-2xl max-w-[1000px] object-contain"
+    className="sm:w-full rounded-lg shadow-2xl max-w-[1000px] object-contain"
     src="https://raw.githubusercontent.com/code-amin/farm/e44f199181acf0c85490be6b467d83bd3bbc25e4/client/src/images/image0.jpeg"
     alt="Descriptive Alt Text"
   />
